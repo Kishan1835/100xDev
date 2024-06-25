5d29949591d9b75b49d37c469ea6165c3f5b7779
T.S. Hayagriva Kishan <sharmilakishan.2003@gmail.com>
2024-06-25 15:29:56 +0530

    file I/0 and SetTimeout async

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 //
 
 
-var g = new Promise(function (resolve) {
-    setTimeout(function () {
-        resolve("foo")
-    }, 1000)
+// var g = new Promise(function (resolve) {
+//     setTimeout(function () {
+//         resolve("foo")
+//     }, 1000)
+// })
+
+// function callback() {
+//     console.log(g)
+// }
+
+// console.log(g)
+// g.then(callback)
+
+
+let fs = require("fs")
+
+fs.readFile("a.txt", function (err, data) {
+    if (err) {
+        console.log("error")
+    }
+    let newdata = data.toString().trim() + "hello kishan"
+    console.log(newdata)
+    fs.writeFile("a.txt", newdata, function (err) {
+        if (err) {
+            console.log("some error ")
+        }
+        console.log("success")
+    })
 })
-
-function callback() {
-    console.log(g)
-}
-
-console.log(g)
-g.then(callback)
