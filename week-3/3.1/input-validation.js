82f3042be24e71342a5874ee17c29796c3a2412d
T.S. Hayagriva Kishan <sharmilakishan.2003@gmail.com>
2024-07-19 15:03:28 +0530

    3.1 middlewares done

--- week-3/3.1/input-validation.js
+++ week-3/3.1/input-validation.js
@@ -26,13 +26,40 @@ const schema = zod.array(zod.number())
 app.use(express.json())
 
 
-app.post("/health-checkup", (req, res) => {
-    const kidenys = req.body.kidenys
-    const response = schema.safeParse(kidenys)
+// app.post("/health-checkup", (req, res) => {
+//     const kidenys = req.body.kidenys
+//     const response = schema.safeParse(kidenys)
 
-    res.send({
-        response
+//     res.send({
+//         response
+//     })
+// })
+
+// app.listen(3000)
+
+
+function validationInput(obj) {
+    const schema = zod.object({
+        email: zod.string().email(),
+        password: zod.string().min(8)
     })
+
+    const response = schema.safeParse(obj);
+    console.log(response)
+} 
+
+validationInput({
+    email: "kishan@gmail.comm",
+    password: "235fdsff4"
 })
 
-app.listen(3000)
+// Valdation in an http request in 
+app.get("/loginjs", (req, res) => {
+    const response = validationInput(req.obj)
+    if (!response.success) {
+        res.json({
+            msg: "not valid input"
+        })
+        return
+    }
+})
