152b6166a52dfa724f92cca285561c8406237a1d
T.S. Hayagriva Kishan <sharmilakishan.2003@gmail.com>
2024-11-30 23:29:06 +0530

    tailwind basic

---
+++ week-8/src/components/RevenueCard.jsx
@@ -0,0 +1,44 @@
+export const RevenueCard = ({
+    title,
+    orderCount,
+    amount
+}) => {
+    return <div className="bg-white rounded shadow-md p-10">
+        <div className="text-gray-700 font-semibold flex gap-x-1 ">
+            <div>
+                {title}
+            </div>
+
+            <div className="flex justify-center flex-col h-full">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
+                </svg>
+            </div>
+
+        </div>
+
+        <div className="flex justify-between pt-2">
+            <div className="font-semibold text-2xl">
+                ₹ {amount}
+            </div>
+
+            {orderCount ? <div className="flex cursor-pointer underline font-medium flex-col justify-center ">
+                <div className="flex">
+                    <div className=" text-blue-600 " >
+                        {orderCount} order(s)
+                    </div>
+
+                    <div>
+                        <svg className="text-blue-600 font-semibold" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                            <path fill-rule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                </div>
+
+
+            </div> : null}
+        </div>
+    </div>
+
+}
+
