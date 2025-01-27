529b6013d2ee876d50fcf8385fdaf27865c1ec57
T.S. Hayagriva Kishan <sharmilakishan.2003@gmail.com>
2025-01-27 23:45:26 +0530

    query all users and creating refrence to specificc userID

--- week-8/payTM/backend/db.js
+++ week-8/payTM/backend/db.js
@@ -1,20 +1,54 @@
+// backend/db.js
 const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost:27017/paytm");
 
-const UserSchema = mongoose.Schema(
-  {
-    username: String,
-    password: String,
-    firstName: String,
-    lastName: String,
+// Create a Schema for Users
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 30,
   },
-  {
-    timestamp: true,
-  }
-);
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+});
 
-const User = mongoose.model("User", UserSchema);
+const accountSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId, // Reference to User model
+    ref: "User",
+    required: true,
+  },
+  balance: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Account = mongoose.model("Account", accountSchema);
+const User = mongoose.model("User", userSchema);
+
 module.exports = {
   User,
+  Account,
 };
