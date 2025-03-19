c4d1763a80be532cde228b78295ca2c50c0b7269
T.S. Hayagriva Kishan <sharmilakishan.2003@gmail.com>
2025-03-19 22:46:29 +0530

    TypeScript interfaces for better type safety and optional properties.

---
+++ week-9/ts-reactApp/src/App.tsx
@@ -0,0 +1,27 @@
+import './App.css'
+function App() {
+  return (
+    <>
+      <Todo
+        title="Go to Gym"
+        description="daily routine"
+        done={false}
+      />
+    </>
+  )
+}
+
+// interface TodoProp{
+//   title: string,
+//   description: string,
+//   done:boolean
+// }
+
+function Todo(props:any) {
+  return <div>
+    <h1>{props.title}</h1>
+    <h2>{props.description}</h2>
+    <h3>{props.done}</h3>
+  </div>
+}
+export default App
