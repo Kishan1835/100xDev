aac74ce6f4b118b813bf834c35c114e64e5a3293
T.S. Hayagriva Kishan <sharmilakishan.2003@gmail.com>
2025-05-10 23:11:21 +0530

    adding:modifications

---
+++ week-9/enums/src/a.tsx
@@ -0,0 +1,10 @@
+
+type keyInput = "up" | "down" | "right" | "left";
+
+function doSomething(keyPressed: keyInput) {
+    // do something
+}
+
+doSomething("up");
+doSomething("down");
+doSomething("pressDown");
