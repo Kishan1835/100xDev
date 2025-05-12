4b6c29091aeb7bd44160b0dea4f83aaf0e829949
T.S. Hayagriva Kishan <sharmilakishan.2003@gmail.com>
2025-05-12 20:08:31 +0530

    what are enums how it works

--- week-9/enums/src/a.tsx
+++ week-9/enums/src/a.tsx
@@ -3,8 +3,11 @@ type keyInput = "up" | "down" | "right" | "left";
 
 function doSomething(keyPressed: keyInput) {
     // do something
+    console.log("done");
+    
 }
 
 doSomething("up");
 doSomething("down");
-doSomething("pressDown");
+// doSomething("pressDown");
+// as the keyInputs as set the types any other tyes can't be entered 
